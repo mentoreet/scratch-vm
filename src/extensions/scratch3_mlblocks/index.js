603d56bea9a6e267f3003ec46a630e73eb927261
mentoreet <brkang@mentoreet.com>
2019-04-23 10:56:34 +0900

    이미지 ML 블럭 추가

--- src/extensions/scratch3_mlblocks/index.js
+++ src/extensions/scratch3_mlblocks/index.js
@@ -14,13 +14,74 @@ class Scratch3MlBlocks {
             name: 'ML Blocks',
             blocks: [
                 {
-                    opcode: 'writeLog',
+                    opcode: 'sendTextToAI',
                     blockType: BlockType.REPORTER,
-                    text: 'AI에게 전달하기 [TEXT]',
+                    text: 'AI에게 텍스트 전달하기 [TEXT]',
                     arguments: {
                         TEXT: {
                             type: ArgumentType.STRING,
                             defaultValue: "hello"
+                        }
+                    }
+                },
+                {
+                    opcode: 'sendImageToAI',
+                    blockType: BlockType.REPORTER,
+                    text: 'AI에게 사진찍어 보내기'
+                },{
+                    opcode: 'select1Of2',
+                    blockType: BlockType.REPORTER,
+                    text: '[TEXT1], [TEXT2] 중에 하나 고르기',
+                    arguments: {
+                        TEXT1: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "항목1"
+                        },
+                        TEXT2: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "항목2"
+                        }
+                    }
+                },                
+                {
+                    opcode: 'select1Of3',
+                    blockType: BlockType.REPORTER,
+                    text: '[TEXT1], [TEXT2], [TEXT3] 중에 하나 고르기',
+                    arguments: {
+                        TEXT1: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "항목1"
+                        },
+                        TEXT2: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "항목2"
+                        },
+                        TEXT3: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "항목3"
+                        }
+                    }
+                },
+                {
+                    opcode: 'select1Of4',
+                    blockType: BlockType.REPORTER,
+                    text: '[TEXT1], [TEXT2], [TEXT3], [TEXT4] 중에 하나 고르기',
+                    arguments: {
+                        TEXT1: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "항목1"
+                        },
+                        TEXT2: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "항목2"
+                        },
+                        TEXT3: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "항목3"
+                        },
+                        TEXT4: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "항목4"
                         }
                     }
                 }
@@ -30,7 +91,7 @@ class Scratch3MlBlocks {
         };
     }
 
-    writeLog (args) {
+    sendTextToAI (args) {
         const text = Cast.toString(args.TEXT);
         log.log(text);
         var prediction = '';
@@ -118,6 +179,73 @@ class Scratch3MlBlocks {
         // }, 1000);  
         
         // return 'test';
+    }
+
+    sendImageToAI (args) {
+        var prediction = '';
+        // parent에 postMessage 날리고,
+        // 여기서는 이벤트 리스너 등록하고, 대기.
+        var messageData = new Object();
+        messageData.type = 'get';
+        messageData.func = 'predict_image';
+        parent.postMessage(messageData,"*");
+
+        var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+        var eventer = window[eventMethod];
+        var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+        // Listen to message from child window
+        eventer(messageEvent, function (e) {
+            console.log('iframe received message!:  ', e);
+            console.log('e.origin ==>' + e.origin);
+            console.log('e.data ==>' + e.data);
+           
+            var jsonData = JSON.parse(e.data);
+            console.log(jsonData);
+            console.log(jsonData.prediction);
+            prediction = jsonData.prediction;
+        }, false);
+
+        var delay = function(time) {
+            return new Promise(function(resolve, reject){
+                setTimeout(resolve, time);
+            });
+        };
+
+        return delay(10000)
+        .then(function(){
+            console.log('return value =>' + prediction);
+            return prediction;
+        });        
+    }
+
+    select1Of4 (args) {
+        const text1 = Cast.toString(args.TEXT1);
+        const text2 = Cast.toString(args.TEXT2);
+        const text3 = Cast.toString(args.TEXT3);
+        const text4 = Cast.toString(args.TEXT4);
+        log.log(text1 + "," + text2 + "," + text3 + "," + text4);
+        var textArray = [text1, text2, text3, text4];
+        var index = Math.floor((Math.random() * 10)) % 4;
+        return textArray[index];
+    }
+
+    select1Of3 (args) {
+        const text1 = Cast.toString(args.TEXT1);
+        const text2 = Cast.toString(args.TEXT2);
+        const text3 = Cast.toString(args.TEXT3);
+        log.log(text1 + "," + text2 + "," + text3);
+        var textArray = [text1, text2, text3];
+        var index = Math.floor((Math.random() * 10)) % 3;
+        return textArray[index];
+    }
+
+    select1Of2 (args) {
+        const text1 = Cast.toString(args.TEXT1);
+        const text2 = Cast.toString(args.TEXT2);
+        log.log(text1 + "," + text2);
+        var textArray = [text1, text2];
+        var index = Math.floor((Math.random() * 10)) % 2;
+        return textArray[index];
     }
 }
 
